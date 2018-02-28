c2c200d3cce481c6360bae3854555d558598c399
paulmarshall <paul@nevada.unr.edu>
2018-02-27 20:33:00 -0700

    Testing to see diff

--- parkr/models.py
+++ parkr/models.py
@@ -9,10 +9,10 @@ class parkingSpace(models.Model):
     def __unicode__(self):
         return 'Parking Space: ' + self.note
 
-    owner = models.CharField(max_length="300",default="0")
-    lat = models.CharField(max_length="300",default="0")
-    lng = models.CharField(max_length="300",default="0")
-    note = models.CharField(max_length="300",default="0")
-    timeOpen = models.CharField(max_length="300",default="0")
-    timeClose = models.CharField(max_length="300",default="0")
+    owner = models.CharField(max_length=300,default="0")
+    lat = models.CharField(max_length=300,default="0")
+    lng = models.CharField(max_length=300,default="0")
+    note = models.CharField(max_length=300,default="0")
+    timeOpen = models.CharField(max_length=300,default="0")
+    timeClose = models.CharField(max_length=300,default="0")
 # Create your models here.
