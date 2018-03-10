b9fd72ccba14a1f4dfa8f161cbdb5193124471a4
convell <paul@nevada.unr.edu>
2018-03-10 14:45:11 -0800

    Fixing some implementation bugs, need the clean function to load the address properly still

--- listing/forms.py
+++ listing/forms.py
@@ -3,25 +3,27 @@ from .models import ParkingSpace
 import googlemaps
 import json
 
-with open("../.creds", "r+") as file:
-    creds = json.load(file.read())
+with open(".creds", "r+") as file:
+    creds = json.loads(file.read())
     gmaps = googlemaps.Client(key=creds["google-api"])
 
 class ParkingSpaceForm(ModelForm):
     class Meta:
         model = ParkingSpace
-        exclude = ('owner', 'timeOpen', 'timeClose', 'lat' ,'lng')
+        fields = ('note',)
+        exclude = ('owner', 'timeOpen', 'timeClose', 'lat','lng')
 
-class ListingForm(ParkingSpaceForm)
+class ListingForm(ParkingSpaceForm):
     address = CharField()
-    class Meta:
-        fields = ProfileForm.Meta.fields + (address)
+    class Meta(ParkingSpaceForm.Meta):
+        fields = ParkingSpaceForm.Meta.fields + ('address',)
 
     def clean_lat(self):
         data = self.cleaned_data.get('address', '')
         geocode_result = gmaps.geocode(data)
         geocode = json.load(geocode_result)
         data = geocode["result"]["geometry"]["location"]["lat"]
+        print (data)
         return data
 
     def clean_lng(self):
@@ -29,4 +31,5 @@ class ListingForm(ParkingSpaceForm)
         geocode_result = gmaps.geocode(data)
         geocode = json.load(geocode_result)
         data = geocode["result"]["geometry"]["location"]["lng"]
+        print (data)
         return data
