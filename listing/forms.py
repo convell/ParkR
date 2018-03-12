610f7ff3114ef9a33582a829540c4ef249eaeecb
paulmarshall <paul@nevada.unr.edu>
2018-03-11 23:12:37 -0600

    Working adress to lat lng translation

--- listing/forms.py
+++ listing/forms.py
@@ -1,11 +1,5 @@
 from django.forms import ModelForm, CharField, HiddenInput
 from .models import ParkingSpace
-import googlemaps
-import json
-
-with open(".creds", "r+") as file:
-    creds = json.loads(file.read())
-    gmaps = googlemaps.Client(key=creds["google-api"])
 
 class ParkingSpaceForm(ModelForm):
 
@@ -19,24 +13,6 @@ class ParkingSpaceForm(ModelForm):
         self.fields['lat'].widget = HiddenInput()
         self.fields['lng'].widget = HiddenInput()
 
-    def clean_lat(self):
-        data = self.cleaned_data.get('address',)
-        print (data)
-        geocode_result = gmaps.geocode(data)
-        geocode = json.load(geocode_result)
-        data = geocode["result"]["geometry"]["location"]["lat"]
-        print (data)
-        return data
-
-    def clean_lng(self):
-        data = self.cleaned_data.get('address', '')
-        print ("whats up")
-        geocode_result = gmaps.geocode(data)
-        geocode = json.load(geocode_result)
-        data = geocode["result"]["geometry"]["location"]["lng"]
-        print (data)
-        return data
-
     def clean(self):
         print("hello")
 
@@ -45,7 +21,24 @@ class ListingForm(ParkingSpaceForm):
     class Meta(ParkingSpaceForm.Meta):
         fields = ParkingSpaceForm.Meta.fields + ('address',)
 
+    #def clean_lat(self):
+    #    data = self.cleaned_data.get('address',)
+    #    print (data)
+    #    geocode_result = gmaps.geocode(data)
+    #    geocode = json.load(geocode_result)
+    #    data = geocode["result"]["geometry"]["location"]["lat"]
+    #    print (data)
+    #    return data
+
+    #def clean_lng(self):
+    #    data = self.cleaned_data.get('address',)
+    #    print ("whats up")
+    #    geocode_result = gmaps.geocode(data)
+    #    geocode = json.load(geocode_result)
+    #    data = geocode["result"]["geometry"]["location"]["lng"]
+    #    print (data)
+    #    return data
+
     def clean(self):
-        super(ParkingSpaceForm, self).clean_lat()
-        #print (cleaned_data)
+        print("hi")
         #return cleaned_data
