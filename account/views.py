f0e3103599124812c35e5ec8847f68b1b08febb3
Phoebe Argon <phoebeargon@gmail.com>
2018-04-26 16:52:20 -0700

    Added review functionality to profile

--- account/views.py
+++ account/views.py
@@ -2,9 +2,11 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 
 from listing.models import ParkingSpace, User
+from .forms import ReviewsForm
+from .models import Reviews
 
 
 # Create your views here.
@@ -17,11 +19,28 @@ def profile(request):
 def show(request, id):
     showUser = get_object_or_404(User, pk=id)
     spaces = ParkingSpace.objects.filter(owner=showUser)
+    reviews = Reviews.objects.filter(reviewed_user=showUser)
     return render(request,
                   "account/show.html",
                   {'owned_spaces': spaces,
-                   'showUser': showUser}
+                   'showUser': showUser,
+                   'reviews' :reviews}
                   )
+
+
+@login_required
+def leave_review(request, id):
+    if request.method == "POST":
+        current_user = get_object_or_404(User, pk=id)
+        review = Reviews(reviewed_user=current_user, reviewing_user=request.user)
+        form = ReviewsForm(instance=review, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('user_show', id=id)
+    else:
+        form = ReviewsForm()
+    return render(request, "account/review_form.html", {'form': form,
+                                                        'id': id})
 
 
 class SignUpView(CreateView):
